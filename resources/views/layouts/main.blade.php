289d883e0961b36fc888d42c2ef5e44e576bef1f
nadliw090801 <muhammadwildan334@gmail.com>
2021-12-01 22:29:26 +0700

    Membuat Halaman Home

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <!-- Bootstrap CSS -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+            crossorigin="anonymous"
+        />
+        <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css"
+        />
+        <script
+            src="https://kit.fontawesome.com/50c83f1bb7.js"
+            crossorigin="anonymous"
+        ></script>
+
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="/css/style.css" />
+        <link rel="shortcut icon" href="/img/logo.ico" />
+
+        <title>VacaSean |</title>
+    </head>
+    <body>
+        @include('partials.navbar')
+        <div class="container">@yield('main')</div>
+        @include('partials.footer')
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossorigin="anonymous"
+        ></script>
+        <script
+            src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"
+            integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
