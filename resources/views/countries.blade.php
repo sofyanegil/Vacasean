0d7fe3e339ad4aecd61c54b251f501cd3bb9c0a1
Sofyan Egi Lesmana <sofyanegil@gmail.com>
2021-12-02 10:46:06 +0700

    Memperbaharui halaman Country

--- resources/views/countries.blade.php
+++ resources/views/countries.blade.php
@@ -3,201 +3,29 @@
 <div class="container">
     <div class="row mt-3">
         <h1 class="mb-3">ASEAN Countries</h1>
-        <div class="col-md-4">
-            <a href="#">
+        @foreach($countries as $country)
+        <div class="col-md-4 mb-4">
+            <a href="{{ $country->slug }}">
                 <div class="card bg-dark text-white">
                     <div
                         class="position-absolute px-3 py-2 text-white"
                         style="background-color: rgba(0, 0, 0, 0.5)"
                     >
-                        <a href="#" class="text-decoration-none text-white"
-                            >Indonesia</a
+                        <a
+                            href="{{ $country->slug }}"
+                            class="text-decoration-none text-white"
+                            >{{ $country->name_country }}</a
                         >
                     </div>
                     <img
-                        src="https://flagcdn.com/h240/id.png"
+                        src="https://flagcdn.com/h240/{{ $country->slug }}.png"
                         height="240"
-                        alt="Indonesia"
+                        alt="{{ $country->name_country}}"
                     />
                 </div>
             </a>
         </div>
-        <div class="col-md-4">
-            <a href="#">
-                <div class="card bg-dark text-white">
-                    <div
-                        class="position-absolute px-3 py-2 text-white"
-                        style="background-color: rgba(0, 0, 0, 0.5)"
-                    >
-                        <a href="#" class="text-decoration-none text-white"
-                            >Brunei</a
-                        >
-                    </div>
-                    <img
-                        src="https://flagcdn.com/h240/bn.png"
-                        height="240"
-                        alt="Brunei"
-                    />
-                </div>
-            </a>
-        </div>
-        <div class="col-md-4">
-            <a href="#">
-                <div class="card bg-dark text-white">
-                    <div
-                        class="position-absolute px-3 py-2 text-white"
-                        style="background-color: rgba(0, 0, 0, 0.5)"
-                    >
-                        <a href="#" class="text-decoration-none text-white"
-                            >Malaysia</a
-                        >
-                    </div>
-                    <img
-                        src="https://flagcdn.com/h240/my.png"
-                        height="240"
-                        alt="Malaysia"
-                    />
-                </div>
-            </a>
-        </div>
-    </div>
-    </div>
-    <div class="row mt-sm-4">
-        <div class="col-md-3">
-            <a href="#">
-                <div class="card bg-dark text-white">
-                    <div
-                        class="position-absolute px-3 py-2 text-white"
-                        style="background-color: rgba(0, 0, 0, 0.5)"
-                    >
-                        <a href="#" class="text-decoration-none text-white"
-                            >Thailand</a
-                        >
-                    </div>
-                    <img
-                        src="https://flagcdn.com/h240/th.png"
-                        height="240"
-                        alt="Thailand"
-                    />
-                </div>
-            </a>
-        </div>
-        <div class="col-md-3">
-            <a href="#">
-                <div class="card bg-dark text-white">
-                    <div
-                        class="position-absolute px-3 py-2 text-white"
-                        style="background-color: rgba(0, 0, 0, 0.5)"
-                    >
-                        <a href="#" class="text-decoration-none text-white"
-                            >Laos</a
-                        >
-                    </div>
-                    <img
-                        src="https://flagcdn.com/h240/la.png"
-                        height="240"
-                        alt="Laos"
-                    />
-                </div>
-            </a>
-        </div>
-        <div class="col-md-3">
-            <a href="#">
-                <div class="card bg-dark text-white">
-                    <div
-                        class="position-absolute px-3 py-2 text-white"
-                        style="background-color: rgba(0, 0, 0, 0.5)"
-                    >
-                        <a href="#" class="text-decoration-none text-white"
-                            >Kamboja</a
-                        >
-                    </div>
-                    <img
-                        src="https://flagcdn.com/h240/kh.png"
-                        height="240"
-                        alt="Kamboja"
-                    />
-                </div>
-            </a>
-        </div>
-        <div class="col-md-3">
-            <a href="#">
-                <div class="card bg-dark text-white">
-                    <div
-                        class="position-absolute px-3 py-2 text-white"
-                        style="background-color: rgba(0, 0, 0, 0.5)"
-                    >
-                        <a href="#" class="text-decoration-none text-white"
-                            >Filipina</a
-                        >
-                    </div>
-                    <img
-                        src="https://flagcdn.com/h240/ph.png"
-                        height="240"
-                        alt="Filipina"
-                    />
-                </div>
-            </a>
-        </div>
-    </div>
-    <div class="row mt-sm-4">
-        <div class="col-md-4">
-            <a href="#">
-                <div class="card bg-dark text-white">
-                    <div
-                        class="position-absolute px-3 py-2 text-white"
-                        style="background-color: rgba(0, 0, 0, 0.5)"
-                    >
-                        <a href="#" class="text-decoration-none text-white"
-                            >Vietnam</a
-                        >
-                    </div>
-                    <img
-                        src="https://flagcdn.com/h240/vn.png"
-                        height="240"
-                        alt="Vietnam"
-                    />
-                </div>
-            </a>
-        </div>
-        <div class="col-md-4">
-            <a href="#">
-                <div class="card bg-dark text-white">
-                    <div
-                        class="position-absolute px-3 py-2 text-white"
-                        style="background-color: rgba(0, 0, 0, 0.5)"
-                    >
-                        <a href="#" class="text-decoration-none text-white"
-                            >Singapura</a
-                        >
-                    </div>
-                    <img
-                        src="https://flagcdn.com/h240/sg.png"
-                        height="240"
-                        alt="Singapura"
-                    />
-                </div>
-            </a>
-        </div>
-        <div class="col-md-4">
-            <a href="#">
-                <div class="card bg-dark text-white">
-                    <div
-                        class="position-absolute px-3 py-2 text-white"
-                        style="background-color: rgba(0, 0, 0, 0.5)"
-                    >
-                        <a href="#" class="text-decoration-none text-white"
-                            >Myanmar</a
-                        >
-                    </div>
-                    <img
-                        src="https://flagcdn.com/h240/mm.png"
-                        height="240"
-                        alt="Myanmar"
-                    />
-                </div>
-            </a>
-        </div>
+        @endforeach
     </div>
 </div>
 @endsection
