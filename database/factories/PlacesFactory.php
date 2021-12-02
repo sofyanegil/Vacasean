1aae40084178b98ed5c9096344fb3f189da8b9a0
SilviFitriawati29 <silvifitriawati29@gmail.com>
2021-12-02 09:25:47 +0700

    Membuat Seeder

--- database/factories/PlacesFactory.php
+++ database/factories/PlacesFactory.php
@@ -14,7 +14,17 @@ class PlacesFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->sentence(mt_rand(1, 4)),
+            'slug' => $this->faker->slug(),
+            'country_id'=> mt_rand(1,10),
+            'city'=>$this->faker->city(),
+            'about'=>collect($this->faker->paragraphs(mt_rand(5, 20)))
+            ->map(
+                fn ($p) => "<p>$p$p$p$p</p>"
+            )->implode(''),
+            'bujur'=>$this->faker->longitude(-180,20),
+            'lintang'=>$this->faker->latitude(-90,90),
+            
         ];
     }
 }
--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Country;
 use Illuminate\Database\Seeder;
 
 class CountrySeeder extends Seeder
@@ -13,6 +15,56 @@ class CountrySeeder extends Seeder
      */
     public function run()
     {
-        //
+        User::factory(20)->create();
+
+        Country::create([
+            'name_country' => 'Indonesia',
+            'slug' => 'id'
+        ]);
+
+        Country::create([
+            'name_country' => 'Malaysia',
+            'slug' => 'my'
+        ]);
+
+        Country::create([
+            'name_country' => 'Singapura',
+            'slug' => 'sg'
+        ]);
+
+        Country::create([
+            'name_country' => 'Vietnam',
+            'slug' => 'vn'
+        ]);
+
+        Country::create([
+            'name_country' => 'Filipina',
+            'slug' => 'ph'
+        ]);
+
+        Country::create([
+            'name_country' => 'Laos',
+            'slug' => 'la'
+        ]);
+
+        Country::create([
+            'name_country' => 'Myanmar',
+            'slug' => 'mm'
+        ]);
+
+        Country::create([
+            'name_country' => 'Brunei',
+            'slug' => 'bn'
+        ]);
+
+        Country::create([
+            'name_country' => 'Kamboja',
+            'slug' => 'kh'
+        ]);
+
+        Country::create([
+            'name_country' => 'Thailand',
+            'slug' => 'th'
+        ]);
     }
 }
